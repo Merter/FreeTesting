b48a823ee330525a95f2752d919d646dffce139b
Merter Sualp <merter.sualp@gmail.com>
2016-03-01 06:38:52 +0200

    My first commit from a iPad to a brand new repo

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    private String message;
+    
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.message = "Hello Ocean!";
+        System.out.println(test.message);
+    }
+}
